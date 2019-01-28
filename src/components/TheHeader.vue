8b362419da0133f2136ba86be2c9b7fad9c62310
Brian Hernandez <brianhernandez@users.noreply.github.com>
2019-01-27 20:09:00 -0500

    Add completed header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="xc-header">
+    <TheSocialbar />
+    <TheNavbar />
+  </header>
+</template>
+
+<script>
+import TheNavbar from "./TheNavbar.vue";
+import TheSocialbar from "./TheSocialbar.vue";
+
+export default {
+  name: "TheHeader",
+  components: {
+    TheNavbar,
+    TheSocialbar
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.xc-header {
+  display: flex;
+  align-items: center;
+  flex-flow: row wrap;
+  background-color: $header-bg-color;
+  padding: 16px 26px;
+
+  @media screen and (min-width: $md) {
+    padding-top: 0px;
+  }
+}
+</style>
