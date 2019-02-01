d5d4bf3f7b861ff89d1ec6d38024dda9ee2ffa64
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-01 17:52:45 -0500

    Add postcss-ordered-values

--- xc-code-challenge/postcss.config.js
+++ xc-code-challenge/postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // Details: Ensure values are ordered consistently in the css. - https://github.com/ben-eb/postcss-ordered-values
+    // Support: Performance - GZIP
+    // Additional Details: Doing so should help gzip as it will help create a more consistent pattern.
+    "postcss-ordered-values": {},
   }
 }
