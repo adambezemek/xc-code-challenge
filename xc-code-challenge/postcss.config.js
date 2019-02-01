cb2d6288cebcdbf9e71f02533136180a363b799b
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-01 17:58:11 -0500

    Update postcss config to use postcss-sorting

--- xc-code-challenge/postcss.config.js
+++ xc-code-challenge/postcss.config.js
@@ -5,5 +5,11 @@ module.exports = {
     // Support: Performance - GZIP
     // Additional Details: Doing so should help gzip as it will help create a more consistent pattern.
     "postcss-ordered-values": {},
+
+    // Details: Sort CSS Properties as defined by `postcssSortingPropertiesOrder`. - https://github.com/hudochenkov/postcss-sorting
+    // Additional Details: Doing so should help gzip as it will help create a more consistent pattern.
+    // Support: Performance - GZIP
+    "postcss-sorting": require("./postcss-sorting.json"),
+
   }
 }
