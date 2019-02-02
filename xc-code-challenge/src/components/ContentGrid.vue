f40beb19226accd515ac3fb7b165aaf1b393ae24
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-01 21:20:16 -0500

    Switch vue-drag-and-drop with vuedraggable
    Seems that even though I'm able to get the example that vue-drag-and-drop to work, it doesn't seem to want to work in this project.

--- xc-code-challenge/src/components/ContentGrid.vue
+++ xc-code-challenge/src/components/ContentGrid.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-    <div class="image" v-for="(image, index) in images" v-on:drag="handleDrag" v-on:drop="handleImageDrop">
-      <img src="" v-bind:src="image.src" v-bind:id="index" v-bind:title="image.name" align="left">
-    </div>
+
+      <draggable v-model="images"  @start="drag=true" @end="drag=false">
+        <div v-for="(image, index) in images" :key="image+index">
+          <img v-bind:src="image.src" >
+        </div>
+      </draggable>
+
   </div>
 </template>
 
+
 <script>
-import 'vue-drag-and-drop';
+import draggable from 'vuedraggable'
 
 export default {
   name: 'ContentGrid',
-
+  components: {
+    draggable
+  },
  data() {
    return {
     currentlyDragging: null,
@@ -20,66 +27,11 @@ export default {
                 {name:'Image 2',src:'http://placehold.it/100/C93742/ffffff'},
                 {name:'Image 3',src:'http://placehold.it/100/6894D1/ffffff'}],
    }
- },
-  methods: {
-    handleDragStart: function (e) {
-      console.log('handleDragStart', e);
-      this.loggedEvent = 'handleDragStart';
-    },
-    handleDragOver: function (e) {
-      console.log('handleDragOver', e);
-      this.loggedEvent = 'handleDragOver';
-    },
-    handleDragEnter: function (e) {
-      console.log('handleDragEnter', e);
-      this.loggedEvent = 'handleDragEnter';
-    },
-    handleDragLeave: function (e) {
-      console.log('handleDragLeave', e);
-      this.loggedEvent = 'handleDragLeave';
-    },
-    handleDragEnd: function (e) {
-      console.log('handleDragEnd', e);
-      this.loggedEvent = 'handleDragEnd';
-    },
-    handleDrop: function (e) {
-      console.log('handleDrop', this.currentlyDragging, e.target);
-      this.currentlyDragging = null;
-      this.loggedEvent = 'handleDrop';
-    },
-    handleImageDrop: function (e) {
-      console.log('handleImageDrop', this.currentlyDragging, e.target);
-      this.currentlyDragging = null;
-      this.loggedEvent = 'handleImageDrop';
-    },
-    handleDrag: function (e) {
-      console.log('handleDrag', e);
-      this.loggedEvent = 'handleDrag';
-      if (!this.currentlyDragging) {
-        this.currentlyDragging = e.srcElement;
-      }
-    }
-  }
+ }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- /* Prevent the text contents of draggable elements from being selectable. */
-    [draggable] {
-      -moz-user-select: none;
-      -khtml-user-select: none;
-      -webkit-user-select: none;
-      user-select: none;
-    }
-    .dragging {
-      opacity: 0.8;
-      color: #6894D1;
-    }
-    .drag-over {
-      outline: 1px solid red;
-    }
-    .drag-enter {
-      color: #C93742;
-    }
+
 </style>
