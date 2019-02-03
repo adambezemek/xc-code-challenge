a64c4b912174b562eb493d38db9880f9ebd570cc
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-03 18:20:47 -0500

    Css cleanup

--- xc-code-challenge/src/main.js
+++ xc-code-challenge/src/main.js
@@ -4,10 +4,13 @@ import "@/css/base/base.css";
 import "@/css/base/typography.css";
 import "@/css/objects/navigation.css";
 import "@/css/theme/xc-code-challenge.css";
+import "@/css/components/skip-navigation.css";
+import "@/css/components/main-navigation.css";
 import "@/css/components/carousel.css";
 import Vue from 'vue'
 import App from './App.vue'
 import "@/css/utilities/overrides/visibility.css";
+import "@/css/utilities/overrides/rotation.css";
 
 Vue.config.productionTip = false
 
