fd6a78edf691d9858d3cfe3e9909c28124456cfc
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-02 21:40:12 -0500

    Add content and url for each section

--- xc-code-challenge/src/components/ContentGrid.vue
+++ xc-code-challenge/src/components/ContentGrid.vue
@@ -30,11 +30,11 @@ export default {
             [{alttext:'A male typing away at his keyboard.',src:'/images/Male-Typing-At-Keyboard.jpg'},
             {alttext:'A female enjoying music while she holds her tablet.',src:'/images/Female-Listening-To-Music.jpg'},
             {alttext:'A female employee being handed a credit card. ',src:'/images/Female-Employee-With-Credit-Card.jpg'}],
-    section: [{name:'Introduction to Habitat'},
-              {name: 'Modules'},
-              {name: 'Getting Started'}]
-   }
- }
+    section: [{name:'Introduction to Habitat', content: '<p>Sitecore Habitat is a range of sites demonstrating the capabilities of the Sitecore Experience Platform.</p><p>The solutions is built on the Sitecore Helix guidelines, which focuses on increasing productivity and quality in Sitecore projects.</p>', url: 'https://example.com'},
+              {name: 'Modules', content: '<p>The habitat example implementation has a range of modules which covers the basics of a Sitecore website.</p>', url: 'https://example.com'},
+              {name: 'Getting Started', content: '<p>Sitecore Helix a defined methodology with conventions and practises - Habitat is an example implementation available for your understanding.</p>', url: 'https://example.com'}]
+    }
+  }
 };
 </script>
 
