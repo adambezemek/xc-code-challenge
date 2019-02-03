f9063af12ce06f8ea1a648c2ebd8bd837a279f87
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-02 21:37:38 -0500

    Refactoring content grid css

--- xc-code-challenge/src/components/ContentGrid.vue
+++ xc-code-challenge/src/components/ContentGrid.vue
@@ -3,8 +3,8 @@
 
   <draggable class="l-grid l-grid--one-third-columns@TabletPortraitMedium" :options="{draggable:'.js-draggable'}" @start="drag=true" @end="drag=false">
     <template v-for="(element, index) in images">
-      <img class="js-draggable" :class="'l-grid__content--header-'+(index+1)" :src="element.src" :alt="element.alttext" :key="index" />
-      <div :key="section[index].name" :class="'l-grid__content--section-'+(index+1)">
+      <img class="js-draggable l-grid__content--header" :src="element.src" :alt="element.alttext" :key="index" />
+      <div :key="section[index].name" class="l-grid__content--section">
       {{section[index].name}}
       </div>
     </template>
@@ -44,31 +44,18 @@ export default {
 .l-grid {
   display: grid;
   grid-gap:1rem;
-  grid-template-areas:
-    "header1"
-    "content1"
-    "header2"
-    "content2"
-    "header3"
-    "content3";
 }
 
-.l-grid__content--header-1{ grid-area: header1; }
-.l-grid__content--header-2{ grid-area: header2; }
-.l-grid__content--header-3{ grid-area: header3; }
-.l-grid__content--section-1{ grid-area: content1; }
-.l-grid__content--section-2{ grid-area: content2; }
-.l-grid__content--section-3{ grid-area: content3; }
 
 
 @media only screen and (min-width: 48em) {
   .l-grid--one-third-columns\@TabletPortraitMedium {
     grid-gap:2rem;
-    grid-template-columns: 360px 360px 360px;
-    grid-template-areas:
-    "header1 header2 header3"
-    "content1 content2 content3";
+    grid-template-columns: repeat(3, minmax(30%, 360px));
   }
+
+  .l-grid__content--header{ grid-area: 1; }
+  .l-grid__content--section{ grid-area: 2; }
 }
 
 </style>
