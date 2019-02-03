e7246a1be4ac1d3e93efccbfb813d5288e9340d0
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-02 23:08:07 -0500

    Begin structure

--- xc-code-challenge/src/App.vue
+++ xc-code-challenge/src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <ContentGrid class="t-background--primary t-color--accent" />
+    <header>
+      <!--
+          Support: Inclusive Design, Accessibility (A11y), Usability
+          Details: The main content is not usually the first thing on a web page. Keyboard and screen reader users generally must navigate a long list
+                    of navigation links, sub-lists of links, corporate icons, site searches, and other elements before ever arriving at the main content.
+                    This is particularly difficult for users with some forms of motor disabilities.
+          Supporting Details: http://webaim.org/techniques/skipnav/
+          Fix: Provide a link at the top of the page to allow a user to skip the navigation and go straight to the main content of the site.
+      -->
+      <a href="#MainContent" class="c-skip-nav u-visually-hidden u-focusable t-background--primary t-color--accent">
+          Skip to main content
+      </a>
+    </header>
+    <main id="MainContent">
+      <ContentGrid class="t-background--primary t-color--accent" />
+    </main>
+    <footer>
+
+    </footer>
   </div>
 </template>
 
