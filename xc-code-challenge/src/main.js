0a7098f617532457c1b4d8ceadd957a6760a81b6
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-02-02 22:36:30 -0500

    Import theme css

--- xc-code-challenge/src/main.js
+++ xc-code-challenge/src/main.js
@@ -1,6 +1,7 @@
 import "@/css/_settings/xc-code-challenge.css";
 import "@/css/base/normalize.css";
 import "@/css/base/base.css";
+import "@/css/theme/xc-code-challenge.css";
 import Vue from 'vue'
 import App from './App.vue'
 import "@/css/utilities/overrides/visibility.css";
