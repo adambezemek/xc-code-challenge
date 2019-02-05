a9e7c2f3643815d0e832875b421a8ce5ce27f721
seandavid1 <seandavid1@gmail.com>
2019-02-04 19:18:01 -0500

    Add files via upload

---
+++ components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="habitat-logo">
+    <img src="~assets/images/Habitat.png" title="Habitat Logo" alt="Habitat Logo">
+  </div>
+</template>
+
+<style>
+.habitat-logo{
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  height: 50px;
+  width: 145px;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+
+@keyframes godown {
+  100% {
+    top: 180px;
+  }
+}
+
+@keyframes goright {
+  100% {
+    left: 70px;
+  }
+}
+</style>
